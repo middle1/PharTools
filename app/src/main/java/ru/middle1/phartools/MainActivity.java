6df002ede9336163b6100737276636c87b331101
Middle1 <rna050906@gmail.com>
2024-07-04 15:19:51 +0300

    General code improvements

--- app/src/main/java/ru/middle1/phartools/MainActivity.java
+++ app/src/main/java/ru/middle1/phartools/MainActivity.java
@@ -1,25 +1,47 @@
 package ru.middle1.phartools;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Instrumentation;
+import android.app.NativeActivity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.net.Uri;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Message;
 import android.telecom.InCallService;
+import android.text.Layout;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.documentfile.provider.DocumentFile;
@@ -40,24 +62,31 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+import javax.microedition.khronos.egl.EGL10;
 import ru.middle1.phartools.Managers.*;
 import ru.middle1.phartools.SettingsWindowActivity;
+import ru.middle1.phartools.Tools.LogUtils;
 import ru.middle1.phartools.Tools.ReadUriPaths;
 
 public class MainActivity extends Activity {
+	private static MainActivity instance;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		FilesManager filesManager = new FilesManager(getContext());
-		//	checkRoot();
 		filesManager.copyAssetFile("unPhar.php", "unPhar.php");
 		filesManager.copyAssetFile("php.ini", "php.ini");
 		filesManager.copyAssetFile("toPhar.php", "toPhar.php");
+		instance = this;
 		Starter();
 	}
-
+    
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
@@ -98,18 +127,30 @@ public class MainActivity extends Activity {
 		return context;
 	}
 
-	public MainActivity getActivity() {
-		return MainActivity.this;
+	public static MainActivity getInstance() {
+		return instance;
 	}
 
 	private void Starter() {
+		Manager manager = new Manager(this, this);
+		try {
+			PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			String version = pInfo.versionName;
+			manager.setSharedPreferences("version", version);
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		String stub = manager.getSharedPreferences("stub");
+		if (stub.isEmpty()) {
+			manager.setSharedPreferences("stub", "__HALT_COMPILER();");
+		}
 		Context con = getContext();
 		FilesManager filesManager = new FilesManager(con);
-		Button LoadFileButton = (Button)findViewById(R.id.load);
-		Button activeButton = (Button)findViewById(R.id.buttonToZip);
-		Button ShowLogButton = (Button)findViewById(R.id.ShowLogButton);
-		Button CopyLogButton = (Button)findViewById(R.id.LogCopyButton);
-		Button ChooseFolderButton = (Button)findViewById(R.id.OutputDirButton);
+		Button LoadFileButton = (Button) findViewById(R.id.load);
+		Button activeButton = (Button) findViewById(R.id.buttonToZip);
+		Button ShowLogButton = (Button) findViewById(R.id.ShowLogButton);
+		Button CopyLogButton = (Button) findViewById(R.id.LogCopyButton);
+		Button ChooseFolderButton = (Button) findViewById(R.id.OutputDirButton);
 		ButtonListeners listener = new ButtonListeners(getContext(), this);
 		listener.ChoosePharButton(LoadFileButton);
 		listener.Active(activeButton);
@@ -117,30 +158,20 @@ public class MainActivity extends Activity {
 		listener.CopyLogButton(CopyLogButton);
 		listener.OutPutDirButton(ChooseFolderButton);
 	}
-	
-	void checkRoot() {
-		try {
-			Runtime.getRuntime().exec("su");
-			} catch (IOException e) {
-			Toast.makeText(getContext(), "Руут не был найден", Toast.LENGTH_LONG)
-			.show();
-		}
-		Toast.makeText(getContext(), "Руут был найден", Toast.LENGTH_LONG).show();
-	}
-	
+
 	public void CreateActivity(int code) {
 		Intent intent = new Intent();
 		switch (code) {
-			case 1:
+		case 1:
 			intent.setType("*/*");
 			intent.setAction(Intent.ACTION_GET_CONTENT);
 			startActivityForResult(Intent.createChooser(intent, "Select file"), code);
 			break;
-			case 2:
+		case 2:
 			intent.setAction(Intent.ACTION_OPEN_DOCUMENT_TREE);
 			startActivityForResult(intent, 2);
 			break;
-			case 3:
+		case 3:
 			intent.setAction(Intent.ACTION_OPEN_DOCUMENT_TREE);
 			startActivityForResult(intent, 3);
 			break;
@@ -161,10 +192,29 @@ public class MainActivity extends Activity {
 		case R.id.action_search:
 			Intent intent = new Intent(MainActivity.this, SettingsWindowActivity.class);
 			startActivity(intent);
-			finish();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
+	}
+
+	@Override
+	public void onBackPressed() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Выход");
+		builder.setMessage("Вы уверены, что хотите выйти из приложения?");
+		builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				finish();
+			}
+		});
+		builder.setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+		builder.show();
 	}
 }
